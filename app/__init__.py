c9303c0e896283ee695da521cfddfabeef69805e
paul2126 <paul.mhryu@gmail.com>
2024-12-04 18:24:16 +0000

    refactor app initialization to load configuration from config.ini; set environment variables for Flask app and environment

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from flask_login import LoginManager
 import os #절대경로를 지정하기 위한 Os모듈 임포트
 from app.extensions import db, login_manager, csrf
 
@@ -8,19 +7,18 @@ import configparser
 
 config = configparser.ConfigParser()
 config.read("config.ini")
-SECRET_KEY = config["APP"]["SECRET_KEY"]
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = SECRET_KEY 
-    basedir = os.path.abspath(os.path.dirname(__file__)) #db파일을 절대경로로 생성
-    dbfile = os.path.join(basedir, 'db.sqlite')#db파일을 절대경로로 생성
-
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile   
-#sqlite를 사용함. (만약 mysql을 사용한다면, id password 등... 더 필요한게많다.)
-    app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True 
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    db_path = os.path.join(basedir, 'db.sqlite')
+    os.environ['FLASK_APP'] = config['FLASK']['FLASK_APP']
+    os.environ['FLASK_ENV'] = config['FLASK']['FLASK_ENV']
+    app.config['SECRET_KEY'] = config["FLASK"]["SECRET_KEY"] 
+    app.config['SQLALCHEMY_DATABASE_URI'] = config['APP']['SQLALCHEMY_DATABASE_URI'] + db_path
+    app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = config['APP']['SQLALCHEMY_COMMIT_ON_TEARDOWN'] 
 #사용자 요청의 끝마다 커밋(데이터베이스에 저장,수정,삭제등의 동작을 쌓아놨던 것들의 실행명령)을 한다.
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = config['APP']['SQLALCHEMY_TRACK_MODIFICATIONS'] 
 #수정사항에 대한 track을 하지 않는다. True로 한다면 warning 메시지유발
     
     from .routes import main
