3d5f2e1fae741701328febbeeb8159882782ab22
paul2126 <paul.mhryu@gmail.com>
2024-12-04 17:41:30 +0000

    update run.py to use default host and port for app.run; comment out previous configuration

--- run.py
+++ run.py
@@ -3,5 +3,6 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True) 
-     #포트번호는 기본 5000, 개발단계에서는 debug는 True
+    # app.run(host='127.0.0.1', port=5000, debug=True)
+    #포트번호는 기본 5000, 개발단계에서는 debug는 True
+    app.run()
